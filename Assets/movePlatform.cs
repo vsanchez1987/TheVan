3266d6b7b83ce935af69f4aa06a405c9fc58e2ac
manmit singh <manmit.singh408@gmail.com>
2014-08-31 12:18:54 -0700

    mock up car scene included

---
+++ Assets/movePlatform.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movePlatform : MonoBehaviour {
+	
+	public float Speed = 1.0F;
+	public float stopper = 50.0F;
+	public float header = 0.0F;
+
+	void Update () 
+	{
+		transform.position += new Vector3(Time.deltaTime * Speed, 0, 0); // apply movement
+		
+		if ((transform.position.x > stopper)) // if we are past either of these then
+		{
+			//transform.position.x= stopper;
+			transform.position -= new Vector3(Time.deltaTime * Speed, 0, 0); // undo the move
+			Speed=-Speed; // go the other way by negating speed
+		}
+		if ((transform.position.x < header)) 
+		{
+			//transform.position.x= header;
+			transform.position -= new Vector3(Time.deltaTime * Speed, 0, 0); // undo the move
+			Speed=-Speed; // go the other way by negating speed
+		}
+
+	}
+
+}
+
+
+
+
