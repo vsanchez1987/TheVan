a6d110b7d67eff2032c1b218f1b9176a1ce143e6
dorichan <dori.p.chan@gmail.com>
2014-08-31 05:29:05 -0700

    Updated van customization menu.

---
+++ Assets/Scripts/VanCustomization.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class VanCustomization : MonoBehaviour 
+{
+	public GameObject item;
+	public Transform target;
+	public bool isOn = false;
+
+	public void OnClick()
+	{
+		isOn = true;
+		item = GameObject.Find ("Chainsaw");
+		Debug.Log (item);
+	}
+
+	void Update()
+	{
+		target = GameObject.Find (gameObject.name + "_gm").transform;
+
+		if (isOn) {
+			item.transform.position = target.transform.position;
+			item.renderer.enabled = true;
+		}
+		isOn = false;
+	}
+}
