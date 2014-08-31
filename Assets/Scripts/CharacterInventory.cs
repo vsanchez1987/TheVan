5242cdfab1723e7b95422085d3512af20d866e9a
dorichan <dori.p.chan@gmail.com>
2014-08-30 23:03:57 -0700

    Added basic inventory script, unfinished van customization scene, and updated Zombie AI script.

---
+++ Assets/Scripts/CharacterInventory.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CharacterInventory: MonoBehaviour 
+{	
+	public GameObject item;
+	public bool withinRange;
+	public List<GameObject> inventory = new List<GameObject>();
+
+	void Start()
+	{
+		item = GameObject.FindWithTag ("Item");
+	}
+
+	void Update()
+	{
+		if (withinRange) {
+			if (Input.GetKeyDown (KeyCode.E)) {
+				inventory.Add(item);
+				item.gameObject.SetActive(false);
+			}
+		}
+	}
+
+	void OnTriggerStay(Collider other)
+	{
+		if (other.gameObject == item) {
+			withinRange = true;
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if (other.gameObject == item) {
+			withinRange = false;
+		}
+	}
+}
