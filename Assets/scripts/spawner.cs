315e09f8bc05ddc8b6e34797d7b4d7337ffc71f4
manmit singh <manmit.singh408@gmail.com>
2014-08-30 22:46:21 -0700

    added spawner script

---
+++ Assets/scripts/spawner.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spawner : MonoBehaviour 
+	{
+		public GameObject spawnedObject;
+		public float numofwaves=1f;
+
+		void Start ()
+		{
+			StartCoroutine(spawn());
+		}
+
+
+		IEnumerator spawn()
+		{
+		if(numofwaves==1){
+			for (int i = 0; i < 3; i++)
+				{
+				Instantiate(spawnedObject, this.transform.position, this.transform.rotation);
+				yield return new WaitForSeconds(1);
+				}
+			numofwaves++;
+			}
+		if(numofwaves==2){
+			for (int i = 0; i < 3; i++)
+			{
+				Instantiate(spawnedObject, this.transform.position, this.transform.rotation);
+				yield return new WaitForSeconds(3);
+			}
+			numofwaves++;
+		}
+		}
+
+
+	}
+
