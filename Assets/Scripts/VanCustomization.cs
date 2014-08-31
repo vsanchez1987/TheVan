104d6fd8638e948addb0d8166fad1fb5a39aae45
dorichan <dori.p.chan@gmail.com>
2014-08-31 12:43:13 -0700

    fixed cv menu bugs

---
+++ Assets/Scripts/VanCustomization.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class VanCustomization : MonoBehaviour 
+{
+	public GameObject item;
+	public Transform target;
+	public bool isOn = false;
+	public Camera vanCam;
+	public Camera mainCamera;
+	public int index;
+	public Text text;
+
+	private CharacterInventory player;
+
+	void Start()
+	{
+		index = 0;
+
+		player = GameObject.FindWithTag ("Player").GetComponent<CharacterInventory>();
+		text = GameObject.Find("Canvas").transform.FindChild("Text").GetComponent<Text> ();
+	}
+	
+	public void OnClick(GameObject _sender)
+	{
+		isOn = true;
+		OnTarget (_sender);
+	}
+	
+	void Update()
+	{
+		if(index <= player.inventory.Capacity) {
+			if(Input.GetKeyDown (KeyCode.RightArrow)) {
+				index++;
+				item = player.inventory[index];
+			}
+			if(Input.GetKeyDown (KeyCode.LeftArrow)) {
+				index--;
+				item = player.inventory[index];
+			}
+		}
+
+		text.text = "Current Item: " + item.ToString();
+	}
+
+	void OnTarget(GameObject _sender)
+	{
+		if (isOn) {
+			target = GameObject.Find (_sender.name + "_gm").transform;
+			item = GameObject.Find(item.name + "_van");
+			item.transform.position = target.transform.position;
+			item.renderer.enabled = true;
+		}
+		isOn = false;
+	}
+
+	public void OnExit()
+	{
+		vanCam.enabled = false;
+		mainCamera.enabled = true;
+	}
+}
---
+++ Assets/Scripts/VanTexture.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class VanTexture : MonoBehaviour 
+{
+	public string wolf = "file://C:/Users/Dori/Desktop/TheVan_copy/Assets/Textures/wolf.jpg";
+	public string cartman = "file://C:/Users/Dori/Desktop/TheVan_copy/Assets/Textures/cartman.png";
+
+	private int clicks = 0;
+
+	public List<string> textures = new List<string>();
+
+	void Start () 
+	{
+		textures.Add (wolf);
+		textures.Add (cartman);
+	}
+
+	public void OnClick()
+	{
+		SwapTexture ();
+		clicks = clicks + 1;
+	}
+
+	void SwapTexture()
+	{
+		WWW www = new WWW(textures[clicks]);
+		gameObject.transform.FindChild("Mesh").renderer.material.mainTexture = www.texture;
+
+		if(clicks == 1) {
+			clicks = -1; // wtf
+		}
+	}
+}
