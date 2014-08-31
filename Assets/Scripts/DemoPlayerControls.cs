ac373a3e3633027d3a8341cff6257f6fc0fc697a
dorichan <dori.p.chan@gmail.com>
2014-08-30 20:49:21 -0700

    Zombie AI Scripts

---
+++ Assets/Scripts/DemoPlayerControls.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DemoPlayerControls : MonoBehaviour 
+{
+	public float speed = 5.0f;
+
+	void Update()
+	{
+		if (Input.GetKey (KeyCode.W)) {
+			transform.Translate(Vector3.forward * speed * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			transform.Translate(-Vector3.right * speed * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			transform.Translate(-Vector3.forward * speed * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			transform.Translate(Vector3.right * speed * Time.deltaTime);
+		}
+
+	}
+}
---
+++ Assets/Scripts/ZombieAIScript.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieAIScript : MonoBehaviour 
+{
+	public Transform target;
+	public State currentState;
+	public float attackRange = 1.0f;
+	public float speed = 5.0f;
+
+	private SphereCollider chaseRadius;
+
+	public enum State {
+		Attack,
+		Idle
+	};
+
+	void Start()
+	{
+		chaseRadius = gameObject.GetComponent<SphereCollider> ();
+		chaseRadius.radius = 5.0f;
+	}
+
+	void Update()
+	{
+		if(currentState == State.Attack) {
+			Vector3 direction = target.transform.position - transform.position;
+			direction.y = 0.0f;
+
+			transform.rotation = Quaternion.Lerp (transform.rotation, Quaternion.LookRotation(-direction),
+			                                      Time.deltaTime);
+
+			if(direction.magnitude < attackRange) {
+				Vector3 moveVector = direction.normalized * speed * Time.deltaTime;
+				transform.position += moveVector;
+			}
+		}
+	}
+
+	void OnTriggerStay (Collider other)
+	{
+		if (other.gameObject == GameObject.Find ("Player")) {
+			target = other.gameObject.transform; 
+			currentState = State.Attack;
+		}
+	}
+
+	void OnTriggerExit (Collider other)
+	{
+		if(other.gameObject == GameObject.Find ("Player")) {
+			target = null;
+			currentState = State.Idle;
+		}
+	}
+}
